f1b977d8dd7293abe8512d47177e1e5228519df8
Ariful Islam Polash <e.arifulislam1793@gmail.com>
2022-07-24 18:29:59 +0600

    complete props in functional component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,14 @@ function App() {
   return (
     <div className="App">
        <h1 className="headingStyle">Todo App</h1>
-       <Card />
+       {/* <Card /> */}
+       <Card todoTitle="First Title" todoDescription="Bangladesh is our homeland.In simple words, it can convert the latest version of JavaScript code into the one that the browser understands. The latest standard version which JavaScript follows is ES2020 " todoFooter="first Footer" />
+       <Card todoTitle="PHP" todoDescription="php is programming languageIn simple words, it can convert the latest version of JavaScript code into the one that the browser understands. The latest standard version which JavaScript follows is ES2020 " todoFooter="php Footer" />
+       <Card todoTitle="Laravel" todoDescription="laravel is php most popular framework.In simple words, it can convert the latest version of JavaScript code into the one that the browser understands. The latest standard version which JavaScript follows is ES2020 " todoFooter="laravel Footer" />
+       <Card todoTitle="React" todoDescription="React.js is javascript library.In simple words, it can convert the latest version of JavaScript code into the one that the browser understands. The latest standard version which JavaScript follows is ES2020 " todoFooter="react Footer" />
+       <Card todoTitle="Angular" todoDescription="Angular.js is javascript library.In simple words, it can convert the latest version of JavaScript code into the one that the browser understands. The latest standard version which JavaScript follows is ES2020 " todoFooter="angular Footer" />
+       <Card todoTitle="Vue" todoDescription="Vue.js is javascript library.In simple words, it can convert the latest version of JavaScript code into the one that the browser understands. The latest standard version which JavaScript follows is ES2020 " todoFooter="vue Footer" />
+       {/* <Card /> */}
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,13 @@
-const todoTitle = "This is Card Title Area";
-const todoDescription="this is card description and create react practicse functional components.description and create react practics.";
-const todoFooter ="This is Card footer Area";
-function Card(){
+// const todoTitle = "This is Card Title Area";
+// const todoDescription="this is card description and create react practicse functional components.description and create react practics.";
+// const todoFooter ="This is Card footer Area";
+
+function Card(props){
     return <div className="card">
-              <h3 className="cardTitle">{todoTitle}</h3>
-              <p className="cardDescription">{todoDescription}</p> 
-              <h3 className="cardFooter">{todoFooter}</h3>
+              <h3 className="cardTitle">{props.todoTitle}</h3>
+              <p className="cardDescription">{props.todoDescription}</p> 
+              <h3 className="cardFooter">{props.todoFooter}</h3>
           </div>
     }
 
-    export default Card;
+export default Card;
